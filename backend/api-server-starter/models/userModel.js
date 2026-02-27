6f6712af7caf6cfc520e684cb023e57ca6047294
MiksuNy <miksunyman1000@gmail.com>
2026-02-27 10:17:35 +0200

    Add user model, controller and router

---
+++ backend/api-server-starter/models/userModel.js
@@ -0,0 +1,15 @@
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true }, // Full name of the user
+        email: { type: String, required: true, unique: true }, // Unique username for login
+        password: { type: String, required: true }, // Hashed password for authentication
+        phone_number: { type: String, required: true }, // Contact phone number
+        gender: { type: String, required: true }, // Gender of the user
+        address: {
+            street: { type: String, required: true }, // Street address
+            city: { type: String, required: true }, // City
+            zipCode: { type: String, required: true } // Postal/ZIP code
+        }
+    },
+    { timestamps: true, versionKey: false }
+);
