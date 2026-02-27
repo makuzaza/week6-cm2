1b687022b227e85269dd85cc8d48e103c75d3d5f
Maria K <makuza.mk@gmail.com>
2026-02-27 20:25:36 +0200

    feat: update localhost with the deployed server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://week6-cm2-ec2u.onrender.com/",
         changeOrigin: true,
       },
     },
